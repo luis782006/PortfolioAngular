6e03636f9974ff8ddf9c095a5106d1c7117dfcf4
luis782006 <luisfelipe782006@gmail.com>
2022-05-18 18:25:33 -0300

    algunas correciones de color

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   
   logoName ="My Portfolio";
-   ventana:boolean=true;
+   ventana:boolean=false;
    
   constructor() { }
 
